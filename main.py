e05f2c58bbb0ccb504951f56750fb907c48503f8
K21002 <K21002kk@aitech.ac.jp>
2022-12-08 15:55:58 +0900

    あかしの日記

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from diaries.DiarySample import DiarySample
+from diaries.AkashiDiary import AkashiDiary
 
 #↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(),]
+diaries = [
+    DiarySample(),
+    AkashiDiary(),
+    ]
 
 for d in diaries:
     print("---------------------------------")

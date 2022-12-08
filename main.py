0cac06f2ebee7b128e7e35e4361a04a58f5d0bff
ritsuki0 <ritsuki20020521@gmail.com>
2022-12-08 15:55:00 +0900

    日記追加

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from diaries.DiarySample import DiarySample
-
+from diaries.K21142Diary import K21142Diary
 #↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(),]
+diaries = [
+    DiarySample(),
+    K21142Diary(),
+]
 
 for d in diaries:
     print("---------------------------------")
